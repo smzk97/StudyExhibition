6495a119f64bb8594d22bf0cedf921c8b2e2f87e
LHX <3109496559@qq.com>
2025-12-05 01:12:06 +0800

    B站/弹幕/逆向

--- 逆向/b站/弹幕数据/逆向/reverse.js
+++ 逆向/b站/弹幕数据/逆向/reverse.js
@@ -1,13 +1,15 @@
 let crypto = require('crypto');
 
 
-function get_main_param(oid,pid,segment_index,label){
+function get_param(oid,pid,label,segment_index = undefined){
     let ts = Math.round(Date.now() / 1e3).toString();
     let v = ''
     if(label === 1)
-        v = `'oid=${oid}&pid=${pid}&segment_index=${segment_index}&type=1&web_location=1315873&wts=${ts}'`
+        v = `oid=${oid}&pe=120000&pid=${pid}&ps=0&pull_mode=1&segment_index=1&type=1&web_location=1315873&wts=${ts}`
+    else if(label === 2)
+        v = `oid=${oid}&pe=360000&pid=${pid}&ps=120000&pull_mode=1&segment_index=1&type=1&web_location=1315873&wts=${ts}`
     else
-        v = `mode=3&oid=${oid}&pagination_str=%7B%22offset%22%3A%22${pagination_str}%22%7D&plat=1&type=1&web_location=1315875&wts=${ts}`;
+        v = `oid=${oid}&pid=${pid}&segment_index=${segment_index}&type=1&web_location=1315873&wts=${ts}`
     let a = (t = "7cd084941338484aae1ad9425b84077c" + "4932caff0ff746eab6f01bf08b70ac45",
             r = [],
             [46, 47, 18, 2, 53, 8, 23, 32, 15, 50, 10, 31, 58, 3, 45, 35, 27, 43, 5, 49, 33, 9, 42, 19, 29, 28, 14, 39, 12, 38, 41, 13, 37, 48, 7, 16, 24, 55, 40, 61, 26, 17, 0, 1, 60, 51, 30, 4, 22, 25, 54, 21, 56, 59, 6, 63, 57, 62, 11, 36, 20, 34, 44, 52].forEach((function(e) {
@@ -20,4 +22,3 @@ function get_main_param(oid,pid,segment_index,label){
     }
 }
 
-console.log(get_main_param())
--- 逆向/b站/弹幕数据/逆向/strat.py
+++ 逆向/b站/弹幕数据/逆向/strat.py
@@ -21,7 +21,8 @@ class Spider:
             'sec-fetch-dest': 'empty',
             'sec-fetch-mode': 'cors',
             'sec-fetch-site': 'same-site',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36'
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36',
+            'cookie': self.cookies
         }
         self.BV = BV
 
@@ -42,13 +43,14 @@ class Spider:
             'sec-fetch-user': '?1',
             'upgrade-insecure-requests': '1',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36',
+            'cookies': self.cookies
         }
         params = {
             'spm_id_from': '333.337.search-card.all.click',
         }
-        response = requests.get('https://www.bilibili.com/video/BV1Cr4y1J7iQ/', params=params, cookies=self.cookies,headers=headers)
-        pid = re.search(r'"cid"\s*:\s*(\d+)', response.text).group(1)
-        oid = re.search(r'"aid"\s*:\s*(\d+)', response.text).group(1)
+        response = requests.get(f'https://www.bilibili.com/video/{self.BV}', params=params,headers=headers)
+        oid = re.search(r'"cid"\s*:\s*(\d+)', response.text).group(1)
+        pid = re.search(r'"aid"\s*:\s*(\d+)', response.text).group(1)
         return {
             'oid': oid,
             'pid': pid
@@ -66,9 +68,60 @@ class Spider:
 
     def crawlBulletComment(self,param):
 
-        param['w_rid'] = self.js_compile.call('get_param')
-        response = requests.get('https://api.bilibili.com/x/v2/dm/wbi/web/seg.so', params=param, cookies=self.cookies,headers=self.headers)
+        # 第一次请求
+        param['segment_index'] = '1'
+        param['pull_mode'] = '1'
+        param['ps'] = '0'
+        param['pe'] = '120000'
+        mid = self.js_compile.call('get_param',param['oid'],param['pid'],1)
+        param['w_rid'] = mid['w_rid']
+        param['wts'] = mid['wts']
+        response = requests.get(url='https://api.bilibili.com/x/v2/dm/wbi/web/seg.so', params=param, headers=self.headers)
+        print('第1次请求成功')
+        print('-'*50)
+        data_list = re.findall(r'([0-9\uFF10-\uFF19\u4e00-\u9fa5\u3000-\u303F\uFF00-\uFFEFa-zA-Z \u3000]+)@', response.text)
+        for data in data_list:
+            with open('1.txt','a',encoding='utf-8') as f:
+                f.write(f'{data}\n')
 
+        # 第二次请求
+        param['ps'] = '120000'
+        param['pe'] = '360000'
+        mid = self.js_compile.call('get_param', param['oid'], param['pid'], 2)
+        param['w_rid'] = mid['w_rid']
+        param['wts'] = mid['wts']
+        response = requests.get('https://api.bilibili.com/x/v2/dm/wbi/web/seg.so', params=param, headers=self.headers)
+        print('第2次请求成功')
+        print('-' * 50)
+        data_list = re.findall(r'([0-9\uFF10-\uFF19\u4e00-\u9fa5\u3000-\u303F\uFF00-\uFFEFa-zA-Z \u3000]+)@',response.text)
+        for data in data_list:
+            with open('1.txt', 'a', encoding='utf-8') as f:
+                f.write(f'{data}\n')
+
+        # n次请求
+        param.pop('pull_mode')
+        param.pop('ps')
+        param.pop('pe')
+        index = 2
+        while True:
+            param['segment_index'] = f'{index}'
+            index += 1
+            mid = self.js_compile.call('get_param', param['oid'], param['pid'], 3)
+            param['w_rid'] = mid['w_rid']
+            param['wts'] = mid['wts']
+            try:
+                response = requests.get('https://api.bilibili.com/x/v2/dm/wbi/web/seg.so', params=param,headers=self.headers)
+                response.raise_for_status()
+                print(f'第{index+1}次请求成功')
+                print('-' * 50)
+                if not response.text:
+                    break
+                data_list = re.findall(r'([0-9\uFF10-\uFF19\u4e00-\u9fa5\u3000-\u303F\uFF00-\uFFEFa-zA-Z \u3000]+)@',response.text)
+                for data in data_list:
+                    with open('1.txt', 'a', encoding='utf-8') as f:
+                        f.write(f'{data}\n')
+            except:
+                break
 
     def run(self):
 
@@ -80,8 +133,8 @@ class Spider:
 if __name__ == '__main__':
 
     config = {
-        'url': '',
-        'cookies': ''
+        'BV': 'BV1FGURBCE8H',
+        'cookies': "buvid3=6AB46122-1114-4AF1-85E4-F05513CF002C94203infoc; b_nut=1761920194; _uuid=A96D8AA8-466B-9D43-EE6A-5B17F597CE31097390infoc; CURRENT_QUALITY=0; buvid_fp=7dc53d52907651242caea95b45648e16; buvid4=F109896E-D277-D4B7-B170-E22348F3794901450-025103122-3s2TtWTpGnrozTZqcWf3JA%3D%3D; rpdid=|(J~lu)YJYkl0J'u~Yumk|k)|; theme-tip-show=SHOWED; theme-avatar-tip-show=SHOWED; home_feed_column=5; browser_resolution=1920-945; DedeUserID=3546580405717300; DedeUserID__ckMd5=443ce1ae9dd8064c; CURRENT_FNVAL=4048; SESSDATA=295d2725%2C1780384945%2C71637%2Ac1CjCCa8-QEnWx8aKAa_QEa54yRqNsljwjuwz5yLufp1S-1ICFLUcofpexpFb2LBE0it0SVldnMEdrNzZGSlg4OEVvdnhZNHJ3bVdBbDdzRUgtVGdkYkM5NHY1SXNFU3J1aHdoQmw4YldkYkRBZGJFZ1JNMlNNTURKRVJGOVhVZFBiWkVTOUF5aWtnIIEC; bili_jct=e363e72e68219a168099b7320dd372e1; bp_t_offset_3546580405717300=1142507617663320064; b_lsid=F10EB7972_19AE9DF5FAB; sid=8nkai4gi; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NjUxMTk1MjIsImlhdCI6MTc2NDg2MDI2MiwicGx0IjotMX0.qYGF06mx5ETjzkKPBoR3ZM5Hhi0QReT3aaQmErivzvM; bili_ticket_expires=1765119462"
     }
-    start = Spider(config['url'],config['cookies'])
+    start = Spider(config['cookies'],config['BV'])
     start.run()
